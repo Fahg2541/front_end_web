e7aa97e7065b7ba0fb12dd37bf7bf846a4e8a67d
Fahg2541 <Fahg2541@gmail.com>
2021-01-18 16:45:09 +0700

    File App.js

--- project/src/App.js
+++ project/src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
+import SiteNavbar from './Components/SiteNavbar';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import HomePage from './Pages/HomePage';
+import Welcome from './Pages/Welcome';
+import React from 'react';
+import Login from './Pages/Login';
+import Basket from './Pages/Basket';
+import Search from './Pages/Search';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <Router>
+        <Navbar />
+        <SiteNavbar />
+
+        <Switch>
+          <Route path="/search">
+            <Search/>
+          </Route>
+          <Route path="/basket">
+            <Basket/>
+          </Route>
+          <Route path="/homepage">
+            <HomePage/>
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Welcome />
+          </Route>
+        </Switch>
+      </Router>
+    )
+  }
 }
 
 export default App;
