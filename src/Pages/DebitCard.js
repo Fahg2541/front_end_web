aec90fc890f23a9582f0d8263b048a24dc423967
Fahg2541 <Fahg2541@gmail.com>
2021-01-21 23:36:14 +0700

    add payment

---
+++ src/Pages/DebitCard.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
+class DebitCard extends React.Component {
+    render() {
+        return (
+            <>
+                <main>
+                    <div class="container-fluid">
+                        <div class="col-sm-8 offset-sm-2 text-center colorlib-heading colorlib-heading-sm">
+                            <h1>Thank you for your order</h1>
+                            <h5>Order number 0123456789012345 your</h5>
+                        </div>
+                    </div>
+                    <div class="container-fluid">
+                        <div class="product-entry border">
+                            <div class="container-fluid">
+                                <div class="col-sm-8 offset-sm-2 text-center colorlib-heading colorlib-heading-sm">
+                                    <h3>Your Delivery Date</h3>
+                                </div>
+                                <div class="col-sm-12 offset-sm-2 text-center colorlib-heading colorlib-heading-sm">
+                                    <div class="row">
+                                        <div class="col-xl-3 col-lg-3 col-md-8 col-sm-8">
+                                            <div class="product-entry border">
+                                                <div class="product-img text-center border">
+                                                    <a class="prod-img" >
+                                                        <img src="assets/image/432.png" alt="" width="185" height="150"></img>
+                                                    </a>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+                                            Est.19 ม.ค.-22 ม.ค.
+                                            </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-xl-3 col-lg-3 col-md-8 col-sm-8">
+                                            <div class="product-entry border">
+                                                <div class="product-img text-center border">
+                                                    <a class="prod-img" >
+                                                        <img src="assets/image/441.png" alt="" width="185" height="150"></img>
+                                                    </a>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+                                            Est.19 ม.ค.-22 ม.ค.
+                                            </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-xl-3 col-lg-3 col-md-8 col-sm-8">
+                                            <div class="product-entry border">
+                                                <div class="product-img text-center border">
+                                                    <a class="prod-img" >
+                                                        <img src="assets/image/481.png" alt="" width="185" height="150"></img>
+                                                    </a>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+                                            Est.19 ม.ค.-22 ม.ค.
+                                            </div>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                    <div class="col-sm-5 offset-sm-2 text-center colorlib-heading colorlib-heading-sm">
+                                        <h4>Order summary &emsp;&emsp; ฿14995.00</h4>
+                                    </div>
+
+                                </div>
+                                <div class="product-caption text-center">
+                                    <Link to="/homepage"><button class="btn btn-secondary" type="button" id="button">shop</button></Link>
+                                </div>
+
+
+
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                </main>
+            </>
+        );
+    }
+}
+export default DebitCard;
--- src/Pages/Payment.js
+++ src/Pages/Payment.js
@@ -1,9 +1,62 @@
 import React from 'react';
 
-class Payment extends React.Component{
+class Payment extends React.Component {
     render() {
         return (
-            <>Payment</>
+            <>
+                <main>
+                    <div class="container-fluid">
+                        <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+                            <div class="carousel-inner">
+                            </div>
+                        </div>
+                        <p> </p>
+
+                        <div class="col-sm-8 offset-sm-2 text-center colorlib-heading colorlib-heading-sm">
+                            <h1>Choose a payment</h1>
+                        </div>
+                    </div>
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-xl-3 col-lg-3 col-md-8 col-sm-8">
+                                <div class="product-entry border">
+                                    <div class="product-img text-center">
+                                        <a class="prod-img" href="/PaymentOnDestination">
+                                            <img src="assets/image/atm1.png" alt="" width="185" height="150"></img>
+                                        </a>
+                                    </div>
+                                    <div class="product-caption text-center">
+                                        <h5>Cash on Delivery</h5>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-lg-3 col-md-8 col-sm-8">
+                                <div class="product-entry border">
+                                    <div class="product-img text-center">
+                                        <a class="prod-img" href="/debit">
+                                            <img src="assets/image/atm1.png" alt="" width="185" height="150"></img>
+                                        </a>
+                                    </div>
+                                    <div class="product-caption text-center">
+                                        <h5>Debit Card</h5>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-5 col-lg-5 col-md-7 col-sm-10">
+                                <div class="product-entry border">
+
+                                    <div class="product-caption text-center">
+                                        <h3>Order summary</h3>
+                                        <p>Total (3 piece) &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; ฿14995.00</p>
+                                        <p>Total &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; ฿14995.00</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </main>
+            </>
         );
     }
 }
