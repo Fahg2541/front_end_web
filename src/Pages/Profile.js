03a02d73a310cfa09299b7d99a196f2e62862e58
sulhan62102405 <sulhansamok@gmail.com>
2021-01-21 15:09:09 +0700

    profile

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,1 +1,70 @@
+import React from 'react';
+
+class Profile extends React.Component{       
+    render() {
+        return (
+                <div class="container-fluid">
+                    <p>Home >> Profile</p>
+
+                    <div class="container">
+                        <div class="main-body"></div>
+                        <div class="row gutters-sm">
+                            <div class="col-md-4 mb-3">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <div class="d-flex flex-column align-items-center text-center">
+                                            
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card mb-3">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-sm-3">
+                                        <h6 class="mb-0">Name : </h6>
+                                    </div>
+                                    <div class="col-sm-9 text-secondary">
+                                       Mejilla 
+                                    </div>
+                                </div>
+                                <hr></hr>
+                                <div class="row">
+                                    <div class="col-sm-3">
+                                        <h6 class="mb-0">Email : </h6>
+                                    </div>
+                                    <div class="col-sm-9 text-secondary">
+                                        mejilla@hotmail.com
+                                    </div>
+                                </div>
+                                <hr></hr>
+                                <div class="row">
+                                    <div class="col-sm-3">
+                                        <h6 class="mb-0">Phone : </h6>
+                                    </div>
+                                    <div class="col-sm-9 text-secondary">
+                                        (+66) 971719806
+                                    </div>
+                                </div>
+                                <hr></hr>
+                                <div class="row">
+                                    <div class="col-sm-3">
+                                        <h6 class="mb-0">Address : </h6>
+                                    </div>
+                                    <div class="col-sm-9 text-secondary">
+                                        หมู่ที่1/120 ตำบลบ้านเป็ด อำเภอเมือง จังหวัดนครศรีธรรมราช 8016
+                                    </div>
+                                </div>
+                            </div>    
+                        </div>
+                        </div>
+                        
+                    </div>
+        );
+    }
+}
+export default Profile;
 
