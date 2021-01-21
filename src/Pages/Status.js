7c9078a3b36729d9bb06982bf590e8707666524d
sulhan62102405 <73519184+sulhan62102405@users.noreply.github.com>
2021-01-22 00:47:59 +0700

    Update Status.js
    status

--- src/Pages/Status.js
+++ src/Pages/Status.js
@@ -4,8 +4,68 @@ import React from 'react';
 class Status extends React.Component{
     render() {
         return (
-            <>Status</>
+            <div class="container-fluid">
+                <p>Home>>Status</p>
+                <div class="tab-content " id="pills-tabContent"  >
+					          {/* <div class="tab-pane border fade show active" id="pills-description" role="tabpanel" aria-labelledby="pills-description-tab"> */}
+                        <div class="row text-center">
+                            <div class="col-1"></div>
+                            <div class="col-2"><h5>Product</h5></div>
+                            <div class="col-4"></div>
+                            <div class="col-2"><h5>Color</h5></div>
+                            <div class="col-2"><h5>Status</h5></div>
+                            
+                        </div>
+                        <hr></hr>
+                    {/* </div> */}
+                </div>
+                <div class="tab-content " id="pills-tabContent">
+                    {/* <div class="tab-pane border fade show active" id="pills-description" role="tabpanel" aria-labelledby="pills-description-tab"> */}
+                        <div class="row text-center">
+                            <div class="col-1"><div class="t"></div></div>
+                            <div class="col-2"><h5><img class="img-fluid" src="assets/image/503.PNG" alt="" width="50%"></img></h5></div>
+                            <div class="col-4"><h5><h5>PUMA Men's Enzo 2 Sneaker</h5></h5></div>
+                            <div class="col-2"><h5><h5>Navy</h5></h5></div>
+                            <div class="col-2"><h5><h5>Wait to pay</h5></h5></div>
+                            
+                        </div>
+                        <hr></hr>
+                    {/* </div> */}
+                </div>
+                <p> </p>
+                <div class="tab-content" id="pills-tabContent">
+                    {/* <div class="tab-pane border fade show active" id="pills-description" role="tabpanel" aria-labelledby="pills-description-tab"> */}
+                        <div class="row text-center">
+                            <div class="col-1"><div class="t"></div></div>
+                            <div class="col-2"><h5><img class="img-fluid" src="assets/image/423.PNG" alt="" width="50%"></img></h5></div>
+                            <div class="col-4"><h5><h5>DREAM PAIRS Oppointed-Ankle Women's Pointed Toe Ankle Strap D'Orsay High Heel Stiletto Pumps Shoes</h5></h5></div>
+                            <div class="col-2"><h5><h5>Blue</h5></h5></div>
+                            <div class="col-2"><h5><h5>Shipping</h5></h5></div>
+                            
+                        </div>
+                        <hr></hr>
+                    {/* </div> */}
+                </div>
+                <p> </p>
+                <div class="tab-content" id="pills-tabContent">
+                    {/* <div class="tab-pane border fade show active" id="pills-description" role="tabpanel" aria-labelledby="pills-description-tab"> */}
+                        <div class="row text-center">
+                            <div class="col-1"><div class="t"></div></div>
+                            <div class="col-2"><h5><img class="img-fluid" src="assets/image/132.PNG" alt="" width="50%"></img></h5></div>
+                            <div class="col-4"><h5><h5>Feethit Mens Slip On Walking Shoes Blade Non Slip Running Shoes Lightweight Breathable Mesh Fashion Sneakers</h5></h5></div>
+                            <div class="col-2"><h5><h5>White</h5></h5></div>
+                            <div class="col-2"><h5><h5>Waiting for delivery</h5></h5></div>
+              
+                        </div>
+                        <hr></hr>
+                    {/* </div> */}
+                </div>
+                
+                
+            </div>
+            
         );
     }
 }
 export default Status;
+
